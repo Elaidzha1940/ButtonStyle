fc6ba5b2a93eebdf48d21e40273ef4ffd0d3ef57
Elaidzha1940 <eli080293@gmail.com>
2023-03-25 17:06:19 +0700

    Multi State Button

---
+++ MultiButtonStyle/MultiButtonStyle/MultiButtonStyleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiButtonStyleApp.swift
+//  MultiButtonStyle
+//
+//  Created by Elaidzha Shchukin on 25.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct MultiButtonStyleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
